ab226d8e9ba8b5947b8ff83e92a81b39130c61ea
Adriano Arruda Ramos <adrianoarrudaramos@gmail.com>
2021-03-22 01:16:36 -0300

    Primeiro commit Instalura-Base

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+// props = {
+//   children: 
+// }
+
+function Title(props) {
+  return <h1>{props.children}</h1>
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <div><Title>Qualquer valor novo</Title> Meu titulo</div>
 }
